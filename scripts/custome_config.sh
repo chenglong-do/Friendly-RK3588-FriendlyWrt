cf0dfa684ff3fadf24f6b49a432578f4fc467473
Chenglong Du <swhite.do@gmail.com>
2026-01-23 16:28:52 +0800

    Update custome_config.sh

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -5,6 +5,7 @@ sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
 
 echo "Replace Rust crates.io registry with mirror"
+find / -path "*/packages/rust/Makefile" 2>/dev/null
 cat packages/rust/Makefile
 sed -i 's/--set=llvm.download-ci-llvm=true/--set=source.crates-io.replace-with=mirror \
 --set=source.mirror.registry=sparse+https:\/\/mirrors.bfsu.edu.cn\/crates.io-index\/ \
